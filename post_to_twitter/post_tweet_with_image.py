e9c4c77064e5c50c52b9bca997c32bac1490a578
Skander Boudawara <skander.boudawara@gmail.com>
2022-07-31 14:14:17 +0200

    posting on twitter

---
+++ post_to_twitter/post_tweet_with_image.py
@@ -0,0 +1,27 @@
+import tweepy
+from get_cred import get_authentification_json
+
+def post_tweet_with_pic(tweet_msg, picture_loc=None):
+    twitter_auth_keys = get_authentification_json()
+    if twitter_auth_keys == {}:
+        print('you need to fill credential.json')
+        return
+    auth = tweepy.OAuthHandler(
+            twitter_auth_keys['consumer_key'],
+            twitter_auth_keys['consumer_secret']
+            )
+    auth.set_access_token(
+            twitter_auth_keys['access_token'],
+            twitter_auth_keys['access_token_secret']
+            )
+    api = tweepy.API(auth)
+
+
+    # Upload image
+    if picture_loc is not None:
+        media = api.media_upload(picture_loc)
+        post_result = api.update_status(status=tweet_msg, media_ids=[media.media_id])
+    else:
+        post_result = api.update_status(status=tweet_msg)
+
+
